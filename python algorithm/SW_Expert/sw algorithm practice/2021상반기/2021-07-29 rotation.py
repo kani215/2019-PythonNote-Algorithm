24883475bd37619beaff9115ee266d6393ed114e
KANI <41600359+kani215@users.noreply.github.com>
2021-08-12 15:06:47 +0900

    Add files via upload

--- python algorithm/SW_Expert/sw algorithm practice/2021상반기/2021-07-29 rotation.py
+++ python algorithm/SW_Expert/sw algorithm practice/2021상반기/2021-07-29 rotation.py
@@ -1,6 +1,22 @@
-MAIN_LIST = [0]*10
+MAIN_LIST = [0] * 10
 COUNT_LIST = [5,5,5,5,5] # not -
 SUM_LIST = [0] * 10
+# input 
+MAIN_LIST = [
+    [1,1,1,1,1  ,1,1,1,1,1],
+    [1,1,1,1,1  ,1,1,1,1,1],
+    [1,1,1,1,1  ,1,1,1,1,1],
+    [1,1,1,1,1  ,1,1,1,1,1],
+    [1,1,1,1,1  ,1,1,1,1,1],
+    [1,1,1,1,1  ,1,1,1,1,1],
+    [1,1,1,1,1  ,1,1,1,1,1],
+    [1,1,1,1,1  ,1,1,1,1,1],
+    [1,1,1,1,1  ,1,1,1,1,1],
+    [1,1,1,1,1  ,1,1,1,1,1],
+]
+# for i in range(10):
+#     MAIN_LIST[i] = list(map(int,input().split()))
+
 def count_check():
     check = 0
     for i in COUNT_LIST:
@@ -9,38 +25,41 @@ def count_check():
     if check == 1:
         print('-1')
     else:
+        print(COUNT_LIST)
         print(25-sum(COUNT_LIST))
 
-def table_check(start,idx):
-    sum1 = int()
-    for i in range(start):
-        sum1 += sum(MAIN_LIST[idx:idx+start])
-    if sum1 == start**2:
-        for j in range(start):
-            SUM_LIST[idx+j] -= 1
-            COUNT_LIST[start-1] -= 1
-        
-# input 
-MAIN_LIST = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-[0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
-[0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
-[0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-[0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-# for i in range(10):
-#     MAIN_LIST[i] = map(int,input().split())
+def table_check(idx, biggest_papper):
+    for loc in range(10):
+        if MAIN_LIST[idx][loc] == 1:
+            sum_check = 0
+
+            for mx in range(biggest_papper):
+                sum_check += sum(MAIN_LIST[idx+mx][loc:loc + biggest_papper])
+            if sum_check == biggest_papper**2:
+                for mx in range(biggest_papper): 
+                    SUM_LIST[idx+mx] -= biggest_papper
+                    for my in range(biggest_papper):
+                        MAIN_LIST[idx+mx][idx+my] -= 1
+                COUNT_LIST[biggest_papper-1] -= 1
+        else:
+            continue
+
+
+    
 # main
 for i in range(10):
     SUM_LIST[i] = sum(MAIN_LIST[i])
-buliet = list(set(SUM_LIST)).sort()
+buliet = list(set(SUM_LIST))
 
 while buliet:
-    start = buliet.pop()
-    for idx in range(10):
-        if SUM_LIST[idx] == buliet:
-            table_check()
-            print(SUM_LIST)
+    biggest_papper = buliet.pop()
+    if biggest_papper < 1:
+        break
+    elif biggest_papper <= 5:
+        for idx in range(10):
+            if SUM_LIST[idx] >= biggest_papper:
+                table_check(idx, biggest_papper)
+                # print(biggest_papper,idx,SUM_LIST)
+                # print(0,0,COUNT_LIST)
+
+count_check()
